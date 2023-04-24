a0e9bce4a9a12bf67a9860552ce9bc655c6c632c
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2023-04-24 09:56:17 -0300

    native base

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider,  } from "native-base";
+import { Loading } from '@components/Loading/Loading';
+import { THEME } from '@theme/index';
 export default function App() {
   const [fontsLoaded] = useFonts([
     Roboto_400Regular,
     Roboto_700Bold
   ])
   return (
-    <View >
-      {fontsLoaded ? <Text>Hello World</Text>: <View/>}
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Hello World</Text>: <Loading/>}
+    </NativeBaseProvider>
 
-
-    </View>
   );
 }
